9193eea2df9dae8b8a36372e1f338ea15a2dafc7
Hale Knox <halelovestrains11@gmail.com>
2017-10-11 12:26:17 -0400

    Update sapi.py
    I'm dumb, fixed issues.

--- tts/sapi.py
+++ tts/sapi.py
@@ -6,9 +6,9 @@ Needs to run on a Windows system
 Requires comtypes
 """
 
-from os import name # Allows checking if using Windows
+import os# Allows checking if using Windows
 try:
-    assert(name() == 'nt') # Checks for Windows
+    assert(os.name== 'nt') # Checks for Windows
 except:
     raise RuntimeError("Windows is required.")
 
