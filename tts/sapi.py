2c340ffcf3eb887a269ad0568ca5458c215e44c1
Jet Fontanilla <jetlogs@gmail.com>
2020-06-23 07:20:31 +0300

    - convert constants into Enums
    - implement SpeechVoiceSpeakFlags available flags

--- tts/sapi.py
+++ tts/sapi.py
@@ -21,7 +21,7 @@ except ImportError:
     stream = comtypes.client.CreateObject("SAPI.SpFileStream")
     from comtypes.gen import SpeechLib
 
-flag_not_xml = 16
+from flags import SpeechVoiceSpeakFlags
 
 class Sapi(object):
     """A speech API using the Microsoft SAPI through COM"""
@@ -89,7 +89,7 @@ class Sapi(object):
             self.voice.AudioOutput = output
         return
 
-    def say(self, message, flag = flag_not_xml):
+    def say(self, message, flag = SpeechVoiceSpeakFlags.Default):
         self.voice.Speak(message, flag)
         return
     
@@ -108,7 +108,7 @@ class Sapi(object):
         stream.Open(filename, SpeechLib.SSFMCreateForWrite)
         return stream
 
-    def create_recording(self, filename, message, flag = flag_not_xml):
+    def create_recording(self, filename, message, flag = SpeechVoiceSpeakFlags.Default):
         """Make a recording of the given message to the file
         The file should be a .wav as the output is
         PCM 22050 Hz 16 bit, Little endianness, Signed"""
